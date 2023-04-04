dd27f9bac10da916315d54bbbebea3c549f26102
Gabriela <93128334+BeastlyGabi@users.noreply.github.com>
2023-04-03 21:46:06 -0400

    mod list file for easier managment
    TODO: Mods Menu

--- source/core/assets/ModHandler.hx
+++ source/core/assets/ModHandler.hx
@@ -13,9 +13,7 @@ typedef ModFormat = {
 class ModHandler {
 	public static var activeMod:ModFormat = null;
 	public static var trackedMods:Array<ModFormat> = [];
-
 	public static var ignoreFolders:Array<String> = [];
-	public static var disregardedFolders:Array<String> = [];
 
 	public static function getPath(?folder:String, ?type:AssetType):String {
 		if (folder != null)
@@ -27,9 +25,11 @@ class ModHandler {
 				modFolder = activeMod.folder;
 		} else {
 			if (trackedMods.length > 0) {
-				for (i in 0...trackedMods.length)
-					if (FileSystem.exists(type.cycleExtensions('mods/${trackedMods[i].folder}/${folder}')))
-						modFolder = trackedMods[i].folder;
+				for (i in 0...trackedMods.length) {
+					if (!ignoreFolders.contains(trackedMods[i].folder))
+						if (FileSystem.exists(type.cycleExtensions('mods/${trackedMods[i].folder}/${folder}')))
+							modFolder = trackedMods[i].folder;
+				}
 			}
 		}
 		if (modFolder != null)
@@ -43,23 +43,35 @@ class ModHandler {
 
 		// read the mods folder
 		if (FileSystem.exists("mods")) {
+			// read the mod order file and see if its enabled
+			var modArray:Array<String> = Utils.readText("mods/order.txt");
+			var modMap:Map<String, Bool> = [];
+			for (i in modArray) {
+				var splitArray:Array<String> = i.split("||");
+				modMap.set(splitArray[0], splitArray[1] == 'true');
+			}
+
+			trace('Mod Status: ${modMap}');
+
 			for (mod in FileSystem.readDirectory("mods")) {
-				var modJson:String = getPath('${mod}/mod', JSON);
+				if (!ignoreFolders.contains(mod) && (modMap.exists(mod) && modMap.get(mod) == true)) {
+					var modJson:String = getPath('${mod}/mod', JSON);
 
-				if (FileSystem.exists(modJson)) {
-					var modJson:ModFormat = cast tjson.TJSON.parse(File.getContent(modJson));
-					var newMod:ModFormat =
-						{
-							name: modJson.name,
-							description: modJson.description,
-							color: modJson.color,
-							folder: mod
-						};
-					newMod.color = FlxColor.fromString(Std.string(modJson.color));
+					if (FileSystem.exists(modJson)) {
+						var modJson:ModFormat = cast tjson.TJSON.parse(File.getContent(modJson));
+						var newMod:ModFormat =
+							{
+								name: modJson.name,
+								description: modJson.description,
+								color: modJson.color,
+								folder: mod
+							};
+						newMod.color = FlxColor.fromString(Std.string(modJson.color));
+						if (modJson.name == null)
+							newMod.name = mod;
 
-					if (modJson.name == null)
-						newMod.name = mod;
-					trackedMods.push(newMod);
+						trackedMods.push(newMod);
+					}
 				}
 			}
 
